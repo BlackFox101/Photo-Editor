2492c85b17a92c37fd2deca02d10c22dadcd40c2
BlackFox101 <sad_alexxxei@mail.ru>
2020-10-16 02:51:07 +0300

    Update

---
+++ tests/changeTextSize.test.ts
@@ -0,0 +1,42 @@
+import {changeTextSize} from '../functions';
+import {Editor, TextBox, Size} from '../type';
+
+describe('changeTextSize function', () => {
+  test('returns Editor', () => {
+    const size: Size = {
+        width: 50,
+        height: 40
+    }
+    const text: TextBox = {
+      coordinates: {
+        x: 10,
+        y: 20,
+      },
+      size: {
+        width: 20,
+        height: 10
+      },
+      color: {
+        r: 10,
+        g: 30,
+        b: 20,
+        a: 1
+      }
+    }
+    const editor: Editor = {
+      canvas: {
+        filter: null,
+        height: 100,
+        width: 100,
+        dedicatedArea: null,
+        artObjects: [],
+        primitives: [],
+        images: [],
+        texts: [text],
+      },
+      selectedObject: null,
+      stateList: null
+    };
+    expect(changeTextSize(editor, text, size)).toBe(editor);
+  });
+})
--- type.ts
+++ type.ts
@@ -1,201 +1,95 @@
 type Editor = {
-  Canvas: Canvas,
-  Filters: Filters,
-  CurrentState: CurrentState
+  canvas: Canvas,
+  selectedObject: SelectedObject,
+  stateList: StateList
 }
 
-enum Filters {
-  Red = '#ff0000',
-  Green = '#00ff00',
-  Blue = '#0000ff',
-  Black = '#000000'
-}
+type SelectedObject = Image | TextBox | ArtObject | Primitive
 
 type StateList = {
-  Undo: Undo,
-  Redo: Redo
+  undo: Undo,
+  redo: Redo
 }
 
 type Undo = {
-  stack: []
+  stack: Canvas[]
 }
 type Redo = {
-  stack: []
+  stack: Canvas[]
 }
 
 type Canvas = {
-  SelectedObject: Images | TextBox | ArtObject | Primitive,
-  Height: number,
-  Width: number,
-  Area: Area
+  filter: Filter,
+  height: number,
+  width: number,
+  dedicatedArea: Area,
+  artObjects: ArtObject[],
+  primitives: Primitive[],
+  images: Image[],
+  texts: TextBox[],
 }
 
 type Primitive = Circle | Rectangle | Triangle;
 
-type Images = {
-  Url: string,
-  Coordinates: CoordinatesBox
-  Size: Size
+type Color = {
+  r: number,
+  g: number,
+  b: number,
+  a: number
 }
 
-type TextBox = {
-  Color: string,
-  Coordinates: CoordinatesBox,
-  Size: Size,
-  Text: string
+type ObjectBox = {
+  coordinates: CoordinatesBox,
+  size: Size,
+  color: Color,
 }
 
+type Image  = {
+  url: Url,
+  coordinates: CoordinatesBox,
+  size: Size
+}
 type ArtObject = {
-  Url: string,
-  Coordinates: CoordinatesBox,
-  Size: Size
+  url: Url,
+  coordinates: CoordinatesBox,
+  size: Size
 }
+type Circle = ObjectBox
+type Rectangle = ObjectBox
+type Triangle = ObjectBox
+type Area = ObjectBox
+type TextBox = ObjectBox
 
-type Circle = {
-  Color: string,
-  Size: Size,
-  Coordinates: CoordinatesBox
-}
-
-type Rectangle = {
-  Color: string,
-  Size: Size,
-  Coordinates: CoordinatesBox
-}
-
-type Triangle = {
-  Color: string,
-  Size: Size,
-  Coordinates: CoordinatesBox
-}
-
-type Color = string
-
-type Area = {
-  Coordinates: CoordinatesBox,
-  Size: Size,
-  Filter: Filter
-}
-
-type Filter = {
-  colorFilter: string
-}
-
-type CurrentState = {
-  Color: string,
-  Font: string,
-  FontSize: number,
-  PrimitiveSize: number,
-  PrimitivePosition: PrimitivePosition
-}
-
-type PrimitivePosition = {
-  x: number,
-  y: number
-}
-
-function importImages(Canvas: Canvas, img: Images) {
-  return Canvas
-}
-
-function exportImages(Canvas: Canvas) {
-  return Canvas
-}
-
-function selectedArea(Editor: Editor, Area: Area) {
-  Editor.Canvas.Area = Area;
-
-  return Editor
-}
-
-function moveArea(Editor: Editor, Coordinates: CoordinatesBox) {
-  Editor.Canvas.Area.Coordinates = Coordinates;
-  Editor.Canvas.Area.Coordinates = Coordinates;
-
-  return Editor
-}
-
-function circumcisionArea(Editor: Editor, Area: Area) {
-  Editor.Canvas.Area.Size = Area.Size
-  return Editor
-}
-
-function deleteObject(Editor: Editor, Area: Area) {
-  Editor.Canvas.Area
-  return Editor
-}
-
-type Size = {
-  Length: number,
-  Width: number
-}
+type Url = string
+type Filter = string
 
 type CoordinatesBox = {
   x: number,
   y: number
 }
 
-function insertText(Editor: Editor, Text: TextBox) {
-  Editor.Canvas.SelectedObject = Text;
-
-  return Editor
+type Size = {
+  width: number,
+  height: number
 }
 
-function changeTextColor(Editor: Editor, Text: TextBox, Color: Color) {
-  Text.Color = Color;
-  Editor.Canvas.SelectedObject = Text;
-
-  return Text
-}
-
-function changeTextSize(Editor: Editor, Text: TextBox, Size: Size) {
-  Text.Size = Size;
-  Editor.Canvas.SelectedObject = Text;
-
-  return Editor
-}
-
-function transferText(Editor: Editor, Text: TextBox, Coordinates: CoordinatesBox) {
-  Text.Coordinates = Coordinates;
-  Editor.Canvas.SelectedObject = Text;
-
-  return Editor
-}
-
-function insertPrimitive(Editor: Editor, Primitive: Primitive) {
-  Editor.Canvas.SelectedObject = Primitive;
-
-  return Editor
-}
-
-function changePrimitiveSize(Editor: Editor, Primitive: Primitive, Size: Size) {
-  Primitive.Size = Size;
-
-  return Editor
-}
-
-function changePrimitivePosition(Editor: Editor, Primitive: Primitive, Coordinates: CoordinatesBox) {
-  Primitive.Coordinates = Coordinates;
-  Editor.Canvas.SelectedObject = Primitive;
-
-  return Editor
-}
-
-function fillingPrimitive(Editor: Editor, Primitive: Primitive, Color: Color) {
-  Primitive.Color = Color;
-  Editor.Canvas.SelectedObject = Primitive;
-
-  return Editor
-}
-
-function insertArtObject(Editor: Editor, ArtObject: ArtObject) {
-  Editor.Canvas.SelectedObject = ArtObject;
-
-  return Editor
-}
-
-function applyFilter(Editor: Editor, Filter: Filter) {
-  Editor.Canvas.Area.Filter = Filter;
-
-  return Editor
+export {
+  Editor,
+  SelectedObject,
+  StateList,
+  Undo,
+  Redo,
+  Canvas,
+  Primitive,
+  Image,
+  ArtObject,
+  Circle,
+  Rectangle,
+  Triangle,
+  Area,
+  TextBox,
+  CoordinatesBox,
+  Size,
+  Color,
+  Filter
 }
